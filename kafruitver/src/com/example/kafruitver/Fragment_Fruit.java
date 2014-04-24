6194aebdb140d7940a75f350aa6604cda437cadc
Vermo <franvermo@gmail.com>
2014-04-24 16:44:11 +0200

    Adding Expandable list to fragments Fruits and Vegetables

--- kafruitver/src/com/example/kafruitver/Fragment_Fruit.java
+++ kafruitver/src/com/example/kafruitver/Fragment_Fruit.java
@@ -1,25 +1,38 @@
 package com.example.kafruitver;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import com.example.kafruitver.R;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-//import android.support.v7.appcompat.R;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ExpandableListView;
 
 public class Fragment_Fruit extends Fragment {
+	
+	MyApp mApp;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mApp = (MyApp)getActivity().getApplication();
     }
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View mV = inflater.inflate(R.layout.fm_fruit, container, false);
-		TextView tv = (TextView) mV.findViewById(R.id.t_fruit);
-		tv.setText("Tab seleccionada" + "\n\n" + "Frutas");;
+		
+		View mV = inflater.inflate(R.layout.expandible_main, container, false);
+		ExpandableAdapter mAdapter = mApp.getAdapter();
+		ExpandableListView mViewExpandable = mApp.getListView();
+		ArrayList<String> mHeadersFruits = mApp.getHeadersFruits();
+		HashMap<String, List<String>> mChildsFruits = mApp.getChildsFruits();
+		mViewExpandable = (ExpandableListView) mV.findViewById(R.id.expandableList);
+		mAdapter = new ExpandableAdapter(getActivity(), mHeadersFruits, mChildsFruits);
+		mViewExpandable.setAdapter(mAdapter);
+		
 		return mV;
 	}
 	
--- kafruitver/src/com/example/kafruitver/Fragment_Vegetable.java
+++ kafruitver/src/com/example/kafruitver/Fragment_Vegetable.java
@@ -1,26 +1,39 @@
 package com.example.kafruitver;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import com.example.kafruitver.R;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-//import android.support.v7.appcompat.R;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ExpandableListView;
 
 public class Fragment_Vegetable extends Fragment {
+	
+	MyApp mApp;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mApp = (MyApp)getActivity().getApplication();
     }
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View mV = inflater.inflate(R.layout.fm_vegetable, container, false);
-		TextView tv = (TextView) mV.findViewById(R.id.t_vegetable);
-		tv.setText("Tab seleccionada" + "\n\n" + "Verduras");;
+		
+		View mV = inflater.inflate(R.layout.expandible_main, container, false);
+		ExpandableAdapter mAdapter = mApp.getAdapter();
+		ExpandableListView mViewExpandable = mApp.getListView();
+		ArrayList<String> mHeadersVegetables = mApp.getHeadersVegetables();
+		HashMap<String, List<String>> mChildsVegetables = mApp.getChildsVegetables();
+		mViewExpandable = (ExpandableListView) mV.findViewById(R.id.expandableList);
+		mAdapter = new ExpandableAdapter(getActivity(), mHeadersVegetables, mChildsVegetables);
+		mViewExpandable.setAdapter(mAdapter);
+		
 		return mV;
-	}	
+	}
 	
 }
